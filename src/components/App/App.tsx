f1a0f94b281a1f7238c39da36cec2e3ea8c9d282
andriy <and123bar@gmail.com>
2026-01-13 00:30:15 +0200

    bugs...

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import type { Votes} from './types/votes'
-import type { VoteType } from './types/votes'
-import CafeInfo from './components/CafeInfo/CafeInfo'
-import VoteOptions from './components/VoteOptions/VoteOptions'
-import VoteStats from './components/VoteStats/VoteStats'
+import type { Votes} from '../../types/votes'
+import type { VoteType } from '../../types/votes'
+import CafeInfo from '../CafeInfo/CafeInfo'
+import VoteOptions from '../VoteOptions/VoteOptions'
+import VoteStats from '../VoteStats/VoteStats'
+import Notification from '../Notification/Notification'
 import css from './App.module.css'
-import Notification from './components/Notification/Notification'
-
 function App() {
 const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
